0b6de3983a7bbf99552f76655f694057679ca571
unknown <imohgenius@yahoo.com>
2020-03-14 13:59:07 +0100

    new

--- resources/views/datatable.blade.php
+++ resources/views/datatable.blade.php
@@ -35,7 +35,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Home</a>
+        <a href="{{ url('/dashboard') }}" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
@@ -196,7 +196,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('/dashboard') }}">Home</a></li>
               <li class="breadcrumb-item active">DataTables</li>
             </ol>
           </div>
