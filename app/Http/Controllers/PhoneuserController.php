a00ef55d9aa221857f48f856b16f8bd487429a26
unknown <imohgenius@yahoo.com>
2020-05-02 08:31:58 +0100

    block count

--- app/Http/Controllers/PhoneuserController.php
+++ app/Http/Controllers/PhoneuserController.php
@@ -11,8 +11,9 @@ class PhoneuserController extends Controller
     public function data()
     { $data=Phoneuser::count();
         // $block=DB::table('phoneusers')->where('blockedstatus', 1)->count();
-        $block=Phoneuser::where('blockstatus', '1')->count();
-        $unblock=Phoneuser::where('blockstatus', '0')->count();
+        $total=Phoneuser::all()->count();
+        $block=Phoneuser::where('blockstatus', '1')->orWhere('blockstatus2', '1')->orWhere('blockstatus3', '1')->count();
+        $unblock=$total-$block;
        return view('dashboard', compact('data','block','unblock'));
     }
     public function update(Request $request, $id)
